ca5b7b306056c892ac38f66dc37022bf146b20b6
Nicolas Rosal <nicolasrosal98@gmail.com>
2021-11-14 17:20:23 +0000

    Add search bar functionality and additional style for buttons (hover effect)

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -1,17 +1,35 @@
 import BabyName from "./Baby";
+// import { Baby } from "./Baby";
 import babyData from "../babyNamesData.json";
 import { sortBabyArray } from "../utils/sortBabies";
+import { useState } from "react";
 
 export default function MainBody(): JSX.Element {
-  const babyDataSorted = sortBabyArray(babyData);
+  // const [babyNames, setBabyNames] = useState<Baby[]>(sortBabyArray(babyData))
+  const [searchValue, setSearchValue] = useState<string>("")
 
-  const listBabies = babyDataSorted.map((baby, index) => (
+  const selectedBabyNames = sortBabyArray(
+    babyData.filter((baby) => 
+      baby.name.toLowerCase().includes(searchValue.toLowerCase())
+    )
+  );
+
+  const listBabies = selectedBabyNames.map((baby, index) => (
     <BabyName id={baby.id} name={baby.name} sex={baby.sex} key={index} />
   ));
-
+  
   return (
     <div className="body-div">
-      <h2 className="subtitle">Title</h2>
+      <input
+        className="search-bar"
+        value={searchValue}
+        placeholder="Search for a name..."
+        onChange={(event) => {
+          setSearchValue(event.target.value)
+        }
+        }
+      />
+      <h2 className="subtitle">My Favourite Names:</h2>
       {listBabies}
     </div>
   );
